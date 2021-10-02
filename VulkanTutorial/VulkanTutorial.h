a3f93976d1fcfde82e0862121e824bb4f04ecd14
RaiNote <Timothy.Lutz@gmx.de>
2021-10-02 11:25:38 +0200

    Initial Commit following "https://vulkan-tutorial.com/"

---
+++ VulkanTutorial/VulkanTutorial.h
@@ -0,0 +1,26 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
+#include <iostream>
+#include <stb.h>
+#include <cstdlib>
+#include <stdexcept>
+#include <vector>
+#include <nameof.hpp>
+
+#ifdef _DEBUG
+#define ASSERT_VULKAN(val)                                                                                             \
+    if (val != VK_SUCCESS)                                                                                             \
+    {                                                                                                                  \
+        std::cout << "An Error occured: " << nameof::nameof_enum(val) << std::endl;                                    \
+        __debugbreak();                                                                                                \
+    }
+#else
+#define ASSERT_VULKAN(val)
+#endif
